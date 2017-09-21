fbcadf64dc095c53fc24a346582ba3a235243f43
Nick Sonner <nicksonner@gmail.com>
2017-09-21 13:20:16 +1000

    add pages, threads controllers and views

---
+++ app/controllers/threads_controller.rb
@@ -0,0 +1,23 @@
+class ThreadsController < ApplicationController
+
+    def index
+      @threads = Thread.all  
+    end 
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def destroy
+    end
+
+
+end
